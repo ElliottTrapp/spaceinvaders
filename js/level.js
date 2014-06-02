e5e541e927cc75f470696235e198ac5265bdbbfd
Elliott Trapp <elliott.trapp@gmail.com>
2014-06-02 14:53:35 +0100

    Changed sprites coordinates

--- js/level.js
+++ js/level.js
@@ -13,31 +13,31 @@
           [0,0,0,0,0,1,1,0,0,0,0]],
      2:  [[0,0,0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0,0,0,0,0],
           [0,0,2,2,2,2,2,2,2,2,0],
           [0,0,2,2,2,2,2,2,2,2,0],
           [0,0,1,1,1,1,1,1,1,1,0],
           [0,0,1,1,1,1,1,1,1,1,0],
           [0,0,1,1,1,1,1,1,1,1,0],
-          [0,0,1,1,1,1,1,1,1,1,0]] };
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0]] };
 
 //This is the positioning of the elements in the game
 
   var spriteData = {
     //Zelda Alien 1 = Octorok
-    'alien1': { sx: 0,  sy: 0,  w: 30, h: 31, cls: Alien, frames: 2 },
+    'alien1': { sx: 0,  sy: 0,  w: 32, h: 30, cls: Alien, frames: 2 },
    // 'alien2': { sx: 0,  sy: 22, w: 23, h: 18, cls: Alien, frames: 0 },
 
   
-    'player': { sx: 0,  sy: 36, w: 26, h: 28, cls: Player },
+    'player': { sx: 0,  sy: 30, w: 25, h: 37, cls: Player },
 
-    'missile': { sx: 0,  sy: 61, w: 8,  h: 20 , cls: Missile },
+    'missile': { sx: 0,  sy: 67, w: 7,  h: 15, cls: Missile },
 
     //Added missile 2 for the enemy projectile
 
-    'missile2': { sx: 0,  sy: 61, w: 8,  h: 20 , cls: Missile }
+  //  'missile2': { sx: 0,  sy: 61, w: 8,  h: 20 , cls: Missile2 }
   }
 
   function startGame() {
